ca1678570f93f0983906d4bfb03c76e280070b4d
KalayaB <53443923+KalayaB@users.noreply.github.com>
2019-07-29 13:56:55 -0400

    Add files via upload

---
+++ KalayaCode.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:19 AM
+"""
+
+
+
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+    
+multiply(4,5)
+    
+def sum(a,b):
+    sum = a+b
+    print(sum)
+    
+sum(3,9)
+
+def area(a,b,c):
+    area= a*b*c
+    print("The area of the triangle with base 0 and height of 7 =", area)
+area(0.5,7,3)
+
